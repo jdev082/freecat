57d42a5d9c64a12e228dd1c18a8bd3a8111744b6
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2021-11-17 16:14:51 -0600

    Make settings and home accessible from search bar
    'fc:settings' redirects to settings
    'fc:home' redirects to home

--- src/js/dynamic.js
+++ src/js/dynamic.js
@@ -1,4 +1,10 @@
 function webload() {
     const wsite = document.getElementById("searchbox").value
+    if (wsite = fc:settings) {
+        const wsite = "../settings.html"    
+    }
+    if (wsite = fc:home) {
+        const wsite - "../home.html"
+    }
     document.getElementById('frame').src=wsite;
 }
