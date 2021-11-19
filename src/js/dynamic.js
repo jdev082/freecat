8fa29a119ffa4d4e4c6ac57e2bad9f06cece36c7
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2021-11-19 15:44:51 -0600

    Bug #3 Fix

--- src/js/dynamic.js
+++ src/js/dynamic.js
@@ -1,10 +1,10 @@
 function webload() {
-    const wsite = document.getElementById("searchbox").value
-    if (wsite = fc:settings) {
+    let wsite = document.getElementById("searchbox").value
+    if (wsite = "fc:settings") {
         const wsite = "../settings.html"    
     }
-    if (wsite = fc:home) {
-        const wsite - "../home.html"
+    if (wsite = "fc:home") {
+        let wsite - "../home.html"
     }
     document.getElementById('frame').src=wsite;
 }
