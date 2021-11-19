1a0a58351359d61f23ab368902920873ce5e5d18
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2021-11-19 15:49:42 -0600

    Update dynamic.js

--- src/js/dynamic.js
+++ src/js/dynamic.js
@@ -1,9 +1,9 @@
 function webload() {
     let wsite = document.getElementById("searchbox").value
-    if (wsite = "fc:settings") {
+    if (wsite = "fc-settings") {
         const wsite = "../settings.html"    
     }
-    if (wsite = "fc:home") {
+    if (wsite = "fc-home") {
         let wsite = "../home.html"
     }
     document.getElementById('frame').src=wsite;
