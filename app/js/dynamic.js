df66ad5f4e1a6dbe64a45ff5b0d36e19f59bec22
JaydenDev <willsjayden2@gmail.com>
2021-12-05 08:37:41 -0600

    update

--- app/js/dynamic.js
+++ app/js/dynamic.js
@@ -1,4 +1,5 @@
 function webload() {
     let wsite = document.getElementById("searchbox").value;
     document.getElementById('frame').src=wsite;
+    document.getElementById("view").src =wsite;
 }
