7deb3abe2f59578b48fca45c28f585b0b24546ff
JaydenDev <willsjayden2@gmail.com>
2021-12-05 12:03:05 -0600

    1.0-prerelease-1

--- app/js/dynamic.js
+++ app/js/dynamic.js
@@ -1,5 +1,4 @@
 function webload() {
     let wsite = document.getElementById("searchbox").value;
-    document.getElementById('frame').src=wsite;
-    document.getElementById("view").src =wsite;
+    document.getElementById("view").src=wsite;
 }
