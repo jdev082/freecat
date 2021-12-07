c786e236fb3e6d32ed09ac2dd43b5362a8bd29ec
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2021-12-06 19:09:15 -0800

    Use addEventListener for everything

--- app/js/dynamic.js
+++ app/js/dynamic.js
@@ -1,4 +1,5 @@
-function webload() {
+const loadPage=function() {
     let wsite = document.getElementById("searchbox").value;
     document.getElementById("view").src=wsite;
 }
+document.getElementById("load").addEventListener("click",loadPage);
