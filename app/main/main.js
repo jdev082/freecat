35d642f2f63a3bcca247ef4ec22f2d53ba897d2f
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-12-08 14:23:39 +1300

    oops

--- app/main/main.js
+++ app/main/main.js
@@ -44,7 +44,7 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
+app.on("ready", () => {
   createWindow()
 
   app.on('activate', function () {
