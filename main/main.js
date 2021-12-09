cbf6fe2519c2d512eed9c97227b7a58f35dd5e8d
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2021-12-08 17:02:38 -0800

    Add icon (another attempt)
    Let's see if this works. Specific to Windows.

--- main/main.js
+++ main/main.js
@@ -65,6 +65,10 @@ app.whenReady().then(() => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    mainWindow.setAppDetails({
+      appId: Math.random().toString(),
+      appIconPath: "./",
+    })
   })
 })
 
