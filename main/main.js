f90d74a6f2235a4e7ab0ac5e3132f54a130b93d9
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2021-12-08 19:55:36 -0800

    Prettier results, improve js

--- main/main.js
+++ main/main.js
@@ -24,7 +24,7 @@ function createWindow() {
           ? path.join(__dirname, "./icons/seconds.ico")
           : path.join(__dirname, "./icons/seconds.svg"),
       webviewTag: true,
-      preload: path.join(__dirname, "preload.js"),
+      preload: path.join(__dirname, "./preload.js"),
     },
   })
 
