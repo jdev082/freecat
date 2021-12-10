b6091f83246ddbffc41c4dbe54d3418235fb8cc8
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2021-12-09 17:38:50 -0800

    Fix main.js
    Fixes #70

--- main/main.js
+++ main/main.js
@@ -70,7 +70,7 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
+app.on("ready").then(() => {
   createWindow()
 
   app.on("activate", function () {
@@ -88,7 +88,7 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit()
+  if (process.platform == "darwin") app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
