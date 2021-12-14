847090287d639bf6eded6b8d908986805f544e8d
JaydenDev <willsjayden2@gmail.com>
2021-12-14 16:58:36 -0600

    prettier

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,4 +1,4 @@
 function webload() {
-    let wsite = document.getElementById("searchbox").value;
-    document.getElementById("view").src=wsite;
+  let wsite = document.getElementById("searchbox").value
+  document.getElementById("view").src = wsite
 }
