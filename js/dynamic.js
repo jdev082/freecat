baa7ebcd5cd3d535e800c1f5fc376fbcab440a89
JaydenDev <willsjayden2@gmail.com>
2021-12-14 21:03:06 -0600

    prettier

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,6 +1,6 @@
 const loadPage = function () {
-  let wsite = document.getElementById("searchbox").value;
-  document.getElementById("view").src = wsite;
-};
-document.getElementById("load").addEventListener("click", loadPage);
-document.getElementById("searchbox").addEventListener("enter", loadPage);
+  let wsite = document.getElementById("searchbox").value
+  document.getElementById("view").src = wsite
+}
+document.getElementById("load").addEventListener("click", loadPage)
+document.getElementById("searchbox").addEventListener("enter", loadPage)
