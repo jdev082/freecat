e05dffc922324105bfc34c9c4497848d3e8ae43d
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2021-12-14 18:56:02 -0800

    Add last changes

--- main/main.js
+++ main/main.js
@@ -17,13 +17,14 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      // Not used.
+      // preload: path.join(__dirname, "preload.js"),
       webviewTag: true,
     },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname,"../index.html"));
+  mainWindow.loadURL(path.join(__dirname, "../index.html"));
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -32,20 +33,18 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", function() {
-  createWindow();
+app.on("ready", createWindow);
+app.on("activate", function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
-  app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", function () {
-  if(process.platform !== "darwin") {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
