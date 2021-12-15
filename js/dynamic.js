445cee83e3dbc6c5f6200efee6bc011f2eb3454b
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2021-12-15 15:58:26 -0800

    Revert "Revert "Make URL bar update when URL changes""
    This reverts commit b25bd61bf75a4c10ed5a11c9841d0cfae2180acd.

--- js/dynamic.js
+++ js/dynamic.js
@@ -4,3 +4,10 @@ const loadPage = function () {
 };
 document.getElementById("load").addEventListener("click", loadPage);
 document.getElementById("searchbox").addEventListener("enter", loadPage);
+let webview = document.getElementById("view");
+let search = document.getElementById("searchbox");
+webview.addEventListener("did-stop-loading", () => {
+  if (document.activeElement !== search) {
+    search.value = webview.src;
+  }
+});
