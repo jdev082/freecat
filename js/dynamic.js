8c8a961d7670d8b1bfaa43d63ba3f4b865dd277d
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2021-12-15 19:55:42 -0800

    Revert "Merge pull request #103 from hello-smile6/add-homepage-url-improved"
    This reverts commit 1a9de4568d07f91b8b78f46dfc197bbb4e0e9235, reversing
    changes made to e99f12f5a86eaa79b3150a51d3a3eb0434f2fa84.

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,22 +1,13 @@
-window.homepageUrl=window.location.href.replace("index.html","home.html");
 const loadPage = function () {
   let wsite = document.getElementById("searchbox").value;
-  if(wsite=="freecat:homepage") {
-    document.getElementById("view").src=homepageUrl;
-  }
-  else {
-    document.getElementById("view").src = wsite;
-  }
+  document.getElementById("view").src = wsite;
 };
 document.getElementById("load").addEventListener("click", loadPage);
 document.getElementById("searchbox").addEventListener("enter", loadPage);
 let webview = document.getElementById("view");
 let search = document.getElementById("searchbox");
 webview.addEventListener("did-stop-loading", () => {
-  if (document.activeElement !== search && webview.src!==homepageUrl) {
+  if (document.activeElement !== search) {
     search.value = webview.src;
-  }
-  if(webview.src==homepageUrl) {
-    search.value="freecat:homepage";
   }
 });
